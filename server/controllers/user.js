48ef6077804fb7a789a60cb45554be9ef34a022a
thapasya <thapasya.murali@gmail.com>
2020-02-19 01:08:50 +0530

    revert getusers api cndn

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,15 +1,14 @@
 const User = require("../models/users");
-const Organization = require("../models/organizations");
 const { errorHandler, dataHandler } = require("../utils/responseHandler");
 const { generateRandomUser, generateRandomOrganizations } = require("../utils/seed-generator");
 
 module.exports.getUsersByOrganizationId = async function(req, res) {
   const { id } = req.params;
   try {
-    // const { excludeUser } = req.query;
+    const { excludeUser } = req.query;
     const response = (await User.find({
       org: id,
-      // _id: { $ne: excludeUser }
+      _id: { $ne: excludeUser }
     })).map(x => x.toClient());
 
     return dataHandler(
