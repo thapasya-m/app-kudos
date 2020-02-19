139d8e07837252d287de4ca3cde4482fd88fc8cd
thapasya <thapasya.murali@gmail.com>
2020-02-20 01:12:44 +0530

    test file

--- client/src/components/ErrorMsg.test.js
+++ client/src/components/ErrorMsg.test.js
@@ -5,15 +5,16 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ErrorMsg from './ErrorMsg';
 
+let wrapper = {};
+beforeAll(() => {
+  wrapper = shallow(<ErrorMsg value='test' />);
+})
 it('test if prop.value exist', () => {
-  const wrapper = shallow(<ErrorMsg value='test' />);
   expect(wrapper.props().children).toBe('test');
 });
 it('h3 element exist if value is non-empty str', () => {
-  const wrapper = shallow(<ErrorMsg value='test' />);
   expect(wrapper.find('h3.error-message')).toHaveLength(1);
 });
 it('test render to h3.text and prop.value are eq', () => {
-  const wrapper = shallow(<ErrorMsg value='test' />);
   expect(wrapper.props().children).toEqual(wrapper.find('h3.error-message').text());
 });
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 const Header = (props) => {
   let history = useHistory();
+  const { value } = props;
 
-  if (!props.value.username) {
+  if (!value.username) {
     return null;
   }
   const logout = () => {
@@ -15,7 +16,7 @@ const Header = (props) => {
   return (  
     <header className='container-flex-end'>
       <div className='user-info container'>
-        <div>{props.value.username} (Kudos Left:{props.value.kudos})</div>
+        <div>{value.username} (Kudos Left:{value.kudos})</div>
         <button onClick={logout}>Log out</button>
       </div>
     </header>
